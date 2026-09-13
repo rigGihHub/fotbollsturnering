6293502693ae9fa37a39ba0553fe41d1f3d3b362
rigGihHub <rikardekstrom@yahoo.com>
2026-09-13 07:34:26 +0200

    Expose new cup action in admin

--- frontend-next/src/app/admin/page.tsx
+++ frontend-next/src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 import AdminWorkspace from "@/components/admin-workspace";
 import AdminOperations from "@/components/admin-operations";
+import CupCreateLauncher from "@/components/cup-create-launcher";
 
 export default function AdminPage(){
-  return <><AdminWorkspace/><AdminOperations/></>;
+  return <><CupCreateLauncher/><AdminWorkspace/><AdminOperations/></>;
 }
