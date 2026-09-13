7c6c684cd0131f12c1d84b7cc4e6fbca02913d49
rigGihHub <rikardekstrom@yahoo.com>
2026-09-13 08:34:39 +0200

    Mount API wake guard in admin

--- frontend-next/src/app/admin/page.tsx
+++ frontend-next/src/app/admin/page.tsx
@@ -1,7 +1,8 @@
 import AdminWorkspace from "@/components/admin-workspace";
 import AdminOperations from "@/components/admin-operations";
 import CupCreateLauncher from "@/components/cup-create-launcher";
+import ApiWakeGuard from "@/components/api-wake-guard";
 
 export default function AdminPage(){
-  return <><CupCreateLauncher/><AdminWorkspace/><AdminOperations/></>;
+  return <><ApiWakeGuard/><CupCreateLauncher/><AdminWorkspace/><AdminOperations/></>;
 }
