00d9946e79d76f93d587e464fbf9404c752ae4fa
rigGihHub <rikardekstrom@yahoo.com>
2026-09-14 11:13:13 +0200

    Load grouped import review styles

--- frontend-next/src/app/admin/layout.tsx
+++ frontend-next/src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import "../v618-admin.css";
 import "../admin-create.css";
 import "../admin-flow.css";
+import "../admin-groups.css";
 
 export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
