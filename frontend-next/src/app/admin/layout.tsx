a0dbcba655eacce45e5ceb1884ade19a57cf0ab3
rigGihHub <rikardekstrom@yahoo.com>
2026-09-14 17:26:40 +0200

    Load CupNavi v2.6.2 admin UX layer

--- frontend-next/src/app/admin/layout.tsx
+++ frontend-next/src/app/admin/layout.tsx
@@ -5,6 +5,7 @@ import "../admin-groups.css";
 import "../admin-mobile-compact.css";
 import "../admin-restored-ux.css";
 import "../admin-v261.css";
+import "../admin-v262.css";
 
 export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;
